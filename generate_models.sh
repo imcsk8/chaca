1db838c795a18038fb511e320413df2314eed488
Iván Chavero <ichavero@chavero.com.mx>
2025-04-15 11:44:32 -0600

    Update generate models for reaction enum

--- generate_models.sh
+++ generate_models.sh
@@ -11,5 +11,7 @@ diesel print-schema > src/schema.rs
 echo "Generating models"
 diesel_ext -t -s src/schema.rs -m \
   -I "crate::schema::*"           \
+  -I "crate::types::*"            \
+  -M "Reaction ReactionEnum"      \
   -I "rocket::serde::{ Deserialize, Serialize }"       \
   -d "${DERIVE_FLAGS}" > src/models.rs
