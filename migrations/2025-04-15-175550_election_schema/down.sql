60bc1f68a212f90f82ef7b786f26612968ae8cdc
Iván Chavero <ichavero@chavero.com.mx>
2025-04-19 14:06:59 -0600

    Added down for schema migration

---
+++ migrations/2025-04-15-175550_election_schema/down.sql
@@ -0,0 +1,10 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE IF EXISTS cat_State;
+DROP TABLE IF EXISTS cat_Matter;
+DROP TABLE IF EXISTS cat_Positions;
+DROP TABLE IF EXISTS cat_Poder;
+DROP TABLE IF EXISTS cat_District;
+DROP TABLE IF EXISTS Applicant;
+DROP TYPE IF EXISTS sexo CASCADE;
+DROP TYPE IF EXISTS ambito_eleccion CASCADE;
+
