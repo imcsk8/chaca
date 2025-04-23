76eeec860d0b9d32608cd763d5931706142a035b
Iván Chavero <ichavero@chavero.com.mx>
2025-04-23 00:28:53 -0600

    Add sed script for model fields

--- generate_models.sh
+++ generate_models.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-DERIVE_FLAGS="Clone, Debug, Identifiable, Queryable, QueryableByName, Insertable, Serialize, Deserialize"
+DERIVE_FLAGS="Clone, Debug, Identifiable, Queryable, QueryableByName, Selectable, Insertable, Serialize, Deserialize"
 
 echo "Running diesel migrations"
 diesel migration run
 
 echo "Gettting schema from database"
-diesel print-schema > src/schema.rs
+diesel print-schema                                   \
+        | sed -f generate_models/update_datatypes.sed \
+        > src/schema.rs
 
 echo "Generating models"
-diesel_ext -t -s src/schema.rs -m \
-  -I "crate::schema::*"           \
-  -I "crate::types::*"            \
-  -M "Reaction ReactionEnum"      \
+diesel_ext -t -s src/schema.rs -m             \
+  -I "crate::schema::*"                       \
+  -I "crate::types::*"                        \
+  -I "rocket::serde::json::serde_json"        \
+  -I "diesel::serialize::ToSql"               \
+  -I "diesel::deserialize::FromSql"           \
+  -I "diesel::sql_types::Jsonb"               \
+  -I "diesel::AsExpression"                   \
+  -I "serde_json::Value as JsonValue"         \
+  -M "ReactionType Reaction"                  \
+  -M "SexoType Sexo"                          \
+  -M "AmbitoEleccionType AmbitoEleccion"      \
   -I "rocket::serde::{ Deserialize, Serialize }"       \
-  -d "${DERIVE_FLAGS}" > src/models.rs
+  -d "${DERIVE_FLAGS}" | sed -f generate_models/update_model_fields.sed \
+  > src/models.rs
