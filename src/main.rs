247856909764867cf5b7de833bf691adf93fca08
Iván Chavero <ichavero@chavero.com.mx>
2025-04-26 20:22:24 -0600

    Updates for facebook login
    - Add endpoints for user info
    - Add facebook auth endpoints
    - Add endpoint for unauthorized user
    - Use configuration from file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate rocket;
 use rocket::fs::FileServer;
 use rocket::response::Debug;
-use rocket::{launch, routes};
-use rocket::get;
+use rocket::{catch, catchers, launch, routes, uri, Request};
+use rocket::response::Redirect;
 use rocket_dyn_templates::Template;
-//https://rocket.rs/guide/v0.5/requests/
+use rocket::http::Cookie;
+use rocket_oauth2::OAuth2;
+use crate::auth::rocket_uri_macro_facebook_login;
+use crate::claims::{AppState, Facebook};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
 
 type Result<T, E = Debug<diesel::result::Error>> = std::result::Result<T, E>;
 
@@ -18,17 +23,54 @@ pub mod types;
 
 pub static STATIC_FILES_DIR: &str = "www/static";
 
+// Define configuration structs
+#[derive(Debug, Deserialize, Serialize)]
+pub struct JwtConfig {
+    secret: String,
+}
+
 #[launch]
 fn rocket() -> _ {
+   // Load Rocket's configuration (this will look for Rocket.toml)
+    let figment = rocket::Config::figment();
+
+    // Extract JWT configuration from the "jwt" table in Rocket.toml
+    let jwt_config: JwtConfig = figment
+        .extract_inner("jwt")
+        .expect("JWT configuration missing in Rocket.toml");
+
+    let app_state = AppState {
+        http_client: Client::new(),
+        jwt_secret: jwt_config.secret,
+    };
+
     rocket::build()
-		.mount("/", routes![auth::login])
+    .mount("/", routes![auth::login, auth::facebook_login,
+                            auth::facebook_callback])
         .mount(
             "/candidates",
             routes![candidates::add, candidates::delete, candidates::get_json,
-                candidates::get_html, candidates::list_all, candidates::list_by_state],
+                candidates::get_html, candidates::list_all, candidates::list_by_state,
+                candidates::get_current_user],
         )
         .mount("/public", FileServer::from(STATIC_FILES_DIR))
+        .manage(app_state)
+        .register("/", catchers![unauthorized_catcher])
         .attach(Template::fairing())
         .attach(db::ChacaDB::fairing())
+        .attach(OAuth2::<Facebook>::fairing("facebook"))
 
+}
+
+// Catcher that handles auth failures and redirects
+#[catch(403)]
+fn unauthorized_catcher(req: &Request) -> Redirect {
+    // Try to get the cached redirect
+    //req.local_cache(|| Redirect::to(uri!(facebook_login)))
+    // TODO: check the use of cache
+    let uri = req.uri().to_string();
+    let cookies = req.cookies();
+    cookies.add(Cookie::new("login_uri", uri.clone()));
+    println!("REQUEST: \n{:#?}\n", req);
+    Redirect::to(uri!(facebook_login))
 }
