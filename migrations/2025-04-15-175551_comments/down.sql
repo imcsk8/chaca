07698bd198d6c0ac4ffaf7d6717257876e6c9f28
Iván Chavero <ichavero@chavero.com.mx>
2025-04-29 10:16:15 -0600

    Fix comments migration deletion

--- migrations/2025-04-15-175551_comments/down.sql
+++ migrations/2025-04-15-175551_comments/down.sql
@@ -1,4 +1,4 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS comments CASCADE;
 DROP TABLE IF EXISTS comment_reactions;
 DROP TYPE IF EXISTS reaction CASCADE;
