9d962dfb2dd81e5585386aaa3d29d6612c7f18b3
Iván Chavero <ichavero@chavero.com.mx>
2025-05-08 03:40:36 -0600

    Added comments module

---
+++ src/comments.rs
@@ -0,0 +1,124 @@
+use crate::claims::Claims;
+use crate::db::*;
+use crate::models::Comment;
+use crate::schema::comments::dsl::*;
+use crate::types::{Positions, Reaction, Result};
+use diesel::prelude::*;
+use rocket::response::{Debug, Redirect};
+use rocket::response::status::{Created, Custom, NotFound};
+use rocket::serde::json::Json;
+use rocket::serde::uuid::Uuid;
+use rocket::{delete, get, post, put};
+use rocket_dyn_templates::{context, Template};
+use rocket_sync_db_pools::diesel;
+use rocket::http::Status;
+use serde_json::json;
+use rocket::serde::{Deserialize, Serialize};
+use diesel::sql_types::{BigInt, Uuid as SqlUuid};
+use diesel::QueryableByName;
+
+
+/*******************************************************************************
+*                                                                              *
+*                                                                              *
+*                        C O M M E N T  E N D P O I N T S                      *
+*                                                                              *
+*                                                                              *
+********************************************************************************/
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CandidateCommentPayload {
+    pub user_id: Uuid,
+    pub oauth_user_id: String,
+    pub candidate_id: Uuid,
+    pub user_fullname: String,
+    pub comment_text: String,
+}
+
+/// Adds a candidate comment
+#[put("/<candidate_id_req>/comment", format = "json", data = "<candidate_comment>")]
+pub async fn add_comment(
+    candidate_id_req: Uuid,
+    user: Claims,
+    cdb: ChacaDB,
+    candidate_comment: Json<CandidateCommentPayload>
+) -> Result<Custom<String>> {
+    let input_comment = candidate_comment.into_inner();
+
+    cdb.run(move |conn| {
+        use crate::schema::comments::dsl::*;
+        match diesel::insert_into(comments)
+            .values((
+                candidate_id.eq(candidate_id_req),
+                user_id.eq(input_comment.user_id), // TODO: Get this from the database to prevent user injection use the User::load_by_oauth function
+                user_name.eq(user.name),
+                content.eq(input_comment.comment_text)
+            ))
+            .execute(conn)
+            {
+            //TODO: check this option Ok(_) => Ok(Custom(Status::Created, format!("Comment Added"))),
+            Ok(_) => Ok(Custom(Status::Created, format!("Comment Added"))),
+            Err(e) => {
+                // If the user already reacted remove any comment and
+                use log::info;
+                info!("Error this should not happen {}", e);
+                Ok(Custom(Status::Accepted, format!("Error inserting comment...")))
+            },
+        }
+    })
+    .await
+}
+
+
+/// gets a candidate comments
+#[get("/<candidate_id_req>/comments", format = "json", rank=1)]
+pub async fn get_comments(
+    candidate_id_req: Uuid,
+    cdb: ChacaDB,
+) -> Result<Json<Vec<Comment>>, NotFound<String>> {
+
+    let results = cdb
+        .run(move |connection| {
+            crate::schema::comments::dsl::comments
+                .filter(candidate_id.eq(candidate_id_req))
+                .load::<Comment>(connection)
+            .expect("Error loading comments")
+        })
+        .await;
+
+    if results.len() > 0 {
+        Ok(Json(results))
+    } else {
+        Err(NotFound(format!("Could not find comments for user: {}", candidate_id_req)))
+    }
+}
+
+
+/// Removes a candidate comment
+#[delete("/<candidate_id_req>/comment/<comment_id_req>", format = "json", data = "<candidate_comment>")]
+pub async fn delete_comment(
+    candidate_id_req: Uuid,
+    comment_id_req: i32,
+    user: Claims,
+    cdb: ChacaDB,
+    candidate_comment: Json<CandidateCommentPayload>
+) -> Result<Json<String>, NotFound<String>> {
+    let input_comment = candidate_comment.into_inner();
+    let results = cdb.run(move |conn| {
+        use crate::schema::comments::dsl::*;
+        diesel::delete(comments)
+            .filter(candidate_id.eq(&candidate_id_req))
+            .filter(comment_id.eq(&comment_id_req))
+            .filter(user_id.eq(&input_comment.user_id))
+            /*.filter(user_id.eq(&user.uuid)) TODO: set the uuid on the request guard to prevent user injection from frontend User::load_by_oaeth*/
+            .execute(conn)
+    })
+    .await;
+
+    if results.unwrap() == 1 {
+        Ok(Json("Comment removed".to_string()))
+    } else {
+        Err(NotFound("Could not find the comment".to_string()))
+    }
+}
